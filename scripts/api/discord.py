87e2a5ff431fe7a3ebd19709bf18cc5367eaf111
Psychon <49412250+psychon-night@users.noreply.github.com>
2024-02-01 09:20:37 -0800

    New API features
    Added additional API features and re-organised a bit

--- scripts/api/discord.py
+++ scripts/api/discord.py
@@ -40,6 +40,42 @@ class parse_tools():
 
 			return authorContents
 
+### MESSAGE TOOLS ###
+
+async def query_messages(ID):
+	""" Use the Discord API to download the last 50 messages in a channel """
+
+	ENDPOINT = url + "/channels/%s/messages"%ID
+
+	try: response = await loop.run_in_executor(ThreadPoolExecutor(), lambda: requests.get(ENDPOINT, headers=HEADERS))
+	except Exception as err: 
+		print(RED + str(err) + RESET)
+
+		return -1
+	
+	try: return json.loads(response.text)
+	except Exception as err:
+		print(str(err)); return 404
+	
+async def trigger_typing(channel):
+	""" Trigger typing in `channel` \n\n Returns `0` on success \n\n Returns `-1` or `404` on error"""
+
+	ENDPOINT = url + "/channels/{channel}/typing"
+
+	try: response = await loop.run_in_executor(ThreadPoolExecutor(), lambda: requests.get(ENDPOINT, headers=HEADERS))
+	except Exception as err: 
+		print(RED + str(err) + RESET)
+
+		return -1
+	
+	try: return 0
+	except Exception as err:
+		print(str(err)); return 404
+	
+### MEMBER TOOLS ###
+
+### GUILD TOOLS ###
+
 async def get_channel(ID, key):
 	""" Use the Discord API to get channel information """
 
@@ -55,6 +91,7 @@ async def get_channel(ID, key):
 	except Exception as err:
 		print(str(err)); return 404
 	
+
 async def get_guild(ID, key):
 	""" Use the Discord API to get guild information """
 
@@ -70,21 +107,10 @@ async def get_guild(ID, key):
 	except Exception as err:
 		print(str(err)); return 404
 	
-async def get_audit_log(id):
-	ENDPOINT = url + f"/guilds/{id}/audit-logs"
 
-	try: response = await loop.run_in_executor(ThreadPoolExecutor(), lambda: requests.get(ENDPOINT, headers=HEADERS))
-	except Exception as err: 
-		print(RED + str(err) + RESET)
-
-		return -1
-	
-	try: return json.loads(response.text)
-	except Exception as err:
-		print(str(err)); return 404
-	
-async def get_guild_members(id):
-	ENDPOINT = url + f"/guilds/{id}/members"
+async def get_audit_log(guild):
+	""" Get `guild`'s audit log """
+	ENDPOINT = url + f"/guilds/{guild}/audit-logs"
 
 	try: response = await loop.run_in_executor(ThreadPoolExecutor(), lambda: requests.get(ENDPOINT, headers=HEADERS))
 	except Exception as err: 
@@ -96,10 +122,57 @@ async def get_guild_members(id):
 	except Exception as err:
 		print(str(err)); return 404
 
-async def query_messages(ID):
-	""" Use the Discord API to download the last 50 messages in a channel """
 
-	ENDPOINT = url + "/channels/%s/messages"%ID
+async def get_guild_members(guild):
+	"Get `guild`'s members"
+	ENDPOINT = url + f"/guilds/{guild}/members"
+
+	try: response = await loop.run_in_executor(ThreadPoolExecutor(), lambda: requests.get(ENDPOINT, headers=HEADERS))
+	except Exception as err: 
+		print(RED + str(err) + RESET)
+
+		return -1
+	
+	try: return json.loads(response.text)
+	except Exception as err:
+		print(str(err)); return 404
+	
+### GUILD BAN TOOLS ###
+
+async def ban_member(guild, user):
+	""" Ban `user` from `guild` """
+	ENDPOINT = url + f"/guilds/{guild}/bans/{user}"
+
+	try: response = await loop.run_in_executor(ThreadPoolExecutor(), lambda: requests.put(ENDPOINT, headers=HEADERS))
+	except Exception as err: 
+		print(RED + str(err) + RESET)
+
+		return -1
+	
+	try: return json.loads(response.text)
+	except Exception as err:
+		if "Expecting value:" in str(err): return ""
+		print(str(err)); return 404
+
+
+async def unban_member(guild, user):
+	""" Unban `user` from `guild` """
+	ENDPOINT = url + f"/guilds/{guild}/bans/{user}"
+
+	try: response = await loop.run_in_executor(ThreadPoolExecutor(), lambda: requests.delete(ENDPOINT, headers=HEADERS))
+	except Exception as err: 
+		print(RED + str(err) + RESET)
+
+		return -1
+	
+	try: return json.loads(response.text)
+	except Exception as err:
+		print(str(err)); return 404
+
+
+async def get_bans(guild):
+	""" Get `guild`'s bans """
+	ENDPOINT = url + f"/guilds/{guild}/bans"
 
 	try: response = await loop.run_in_executor(ThreadPoolExecutor(), lambda: requests.get(ENDPOINT, headers=HEADERS))
 	except Exception as err: 
