3b941ae67c3e59b9851057ba187448a7b00e4119
SleepyStatic <49412250+psychon-night@users.noreply.github.com>
2025-01-18 13:50:24 -0800

    Bump to 1.24.3

--- resources/shared.py
+++ resources/shared.py
@@ -2,6 +2,7 @@ import discord
 import dotenv
 import os
 import json
+import datetime
 import systemd.journal as systemd
 
 dotenv.load_dotenv(".env")
@@ -92,20 +93,44 @@ INTEGRATION_TYPES_SERVER_ONLY = {discord.IntegrationType.guild_install}
 
 PATH = os.getenv("systemPath")
 
+LOG_SEVERITY = ["EMERGENCY", "ALERT", "CRITICAL", "ERROR", "WARNING", "NOTICE", "INFO"]
+
 class journal:
 	""" Interface with systemd logs """
+	def logDisk(message:str, severity:int=6):
+		LOG_LOC = PATH + "/logs/journal"
+		
+
+		template = "{date}@{time} - [{sev}] {entry}\n"
+
+		current_time = datetime.datetime.now().strftime("%H:%M:%S")
+		today = datetime.date.today()
+
+		if os.path.exists(LOG_LOC):
+			with open(LOG_LOC, "a") as log_file: log_file.write(template.format(date=today, time=current_time, sev=LOG_SEVERITY[severity], entry=message))
+		else:
+			with open(LOG_LOC, "x") as log_file: log_file.write(template.format(date=today, time=current_time, sev=LOG_SEVERITY[severity], entry=message))
+
 	def log(message:str, severity:int=6):
 		""" 
 		Write a log message\n
 		Severity: `0=emergency, alert=1, 2=critical, 3=error, 4=warning, 5=notice, 6=info`
 		"""
 
-		SEVERITY = ["emergency", "alert", "critical", "error", "warning", "notice", "info"]
-
-		systemd.send(message, SYSLOG_IDENTIFIER="fritz", LEVEL=SEVERITY[severity])
+		journal.logDisk(message, severity)
+		systemd.send(message, SYSLOG_IDENTIFIER="fritz", LEVEL=LOG_SEVERITY[severity])
 
 	def log_fatal(message:str):
 		""" Logs at the CRITICAL level """
+		LOG_LOC = PATH + "/logs/journal"
+		template = "{date}@{time} - [CRITICAL] {entry}\n"
+		current_time = datetime.datetime.now().strftime("%H:%M:%S")
+		today = datetime.date.today()
+
+		if os.path.exists(LOG_LOC):
+			with open(LOG_LOC, "a") as log_file: log_file.write(template.format(date=today, time=current_time, entry=message))
+		else:
+			with open(LOG_LOC, "x") as log_file: log_file.write(template.format(date=today, time=current_time, entry=message))
 
 		systemd.send(message, SYSLOG_IDENTIFIER="fritz", LEVEL="critical")
 
