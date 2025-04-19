08d6941a2fd023d096feb0c8d94eb7c6b7be9d06
Theo <49412250+psychon-night@users.noreply.github.com>
2025-04-19 13:30:10 -0700

    AAAAAAAAAA

--- scripts/api/lumos_status.py
+++ scripts/api/lumos_status.py
@@ -61,7 +61,7 @@ async def getServerStatus(ctx, sendPlayerList=False):
 
 			for playerInstance in responseJSON["players"]["list"]:
 				if detectedPlayers == "": detectedPlayers = playerInstance["name"]
-				else: detectedPlayers = ", " + playerInstance["name"]
+				else: detectedPlayers = detectedPlayers + ", " + playerInstance["name"]
 
 		await ctx.respond(f"Server is online!\nIP: `{IP}`\nPlayers: {PLAYER_TEXT}\n{detectedPlayers}\n\n{CACHE_TEXT}")
 
