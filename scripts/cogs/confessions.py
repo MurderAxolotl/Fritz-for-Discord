a5366d7cf8376c38bd0c8822be0d88462bcdfd91
fodfodfod <94200657+fodfodfod@users.noreply.github.com>
2025-12-12 15:38:56 -0500

    improve confession error handling

--- scripts/cogs/confessions.py
+++ scripts/cogs/confessions.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 from resources.shared import CONTEXTS, INTEGRATION_TYPES
 
+import scripts.tools.journal          as journal
+
 class ConfessionView(discord.ui.DesignerView):
 	def __init__(self, text="", *, title=None):
 		super().__init__(timeout=None)
@@ -24,8 +26,12 @@ class Confessions(commands.Cog):
 	### BOT UTILITIES ###
 	@commands.slash_command(name='confess', description='Send an anonymous message to the current channel', contexts=CONTEXTS, integration_types=INTEGRATION_TYPES)
 	async def confess(self, ctx: discord.ApplicationContext, confession=discord.Option(input_type=str, name="your-confession", required=True)):
+
 		try:
-			await ctx.send(view=AboutView(confession, title="Confession"))
-			await ctx.respond(view=AboutView(confession, title="Confession Recieved"), ephemeral=True)
+			await ctx.send(view=ConfessionView(confession, title="Confession"))
+			await ctx.respond(view=ConfessionView(confession, title="Confession Recieved"), ephemeral=True)
 		except:
-			await ctx.respond(view=AboutView("Failed to send confession", title="Something went wrong"), ephemeral=True)
+			try:
+				await ctx.respond(view=ConfessionView("Failed to send confession", title="Something went wrong"), ephemeral=True)
+			except:
+				journal.log("Something really went wrong in confessions")
