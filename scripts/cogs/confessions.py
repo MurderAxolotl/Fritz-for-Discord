424c7b29b58a4208c6cfe2fd62e448eecf1272b0
fodfodfod <94200657+fodfodfod@users.noreply.github.com>
2025-12-12 15:20:08 -0500

    improve confession variable naming

--- scripts/cogs/confessions.py
+++ scripts/cogs/confessions.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from resources.shared import CONTEXTS, INTEGRATION_TYPES
 
-class AboutView(discord.ui.DesignerView):
+class ConfessionView(discord.ui.DesignerView):
 	def __init__(self, text="", *, title=None):
 		super().__init__(timeout=None)
 
@@ -23,9 +23,9 @@ class Confessions(commands.Cog):
 	
 	### BOT UTILITIES ###
 	@commands.slash_command(name='confess', description='Send an anonymous message to the current channel', contexts=CONTEXTS, integration_types=INTEGRATION_TYPES)
-	async def confess(self, ctx: discord.ApplicationContext, option=discord.Option(input_type=str, name="your-confession", required=True)):
+	async def confess(self, ctx: discord.ApplicationContext, confession=discord.Option(input_type=str, name="your-confession", required=True)):
 		try:
-			await ctx.send(view=AboutView(option, title="Confession"))
-			await ctx.respond(view=AboutView(option, title="Confession Recieved"), ephemeral=True)
+			await ctx.send(view=AboutView(confession, title="Confession"))
+			await ctx.respond(view=AboutView(confession, title="Confession Recieved"), ephemeral=True)
 		except:
 			await ctx.respond(view=AboutView("Failed to send confession", title="Something went wrong"), ephemeral=True)
