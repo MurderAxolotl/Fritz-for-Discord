f544038842c19b38a64fe09ffb3584bc0d4c159e
Theo <49412250+MurderAxolotl@users.noreply.github.com>
2026-01-06 14:55:10 -0800

    Make errors ephemeral

--- scripts/errors/commandCheck.py
+++ scripts/errors/commandCheck.py
@@ -16,48 +16,48 @@ from scripts.cogs.management import ManagementView
 async def on_command_error(ctx, error):
 	try:
 		if isinstance(error.original, commands.NotOwner) or isinstance(error, swiperNoSwipingError):
-			await ctx.respond("Only the operator may run this command")
+			await ctx.respond("Only the operator may run this command", ephemeral=True)
 
 		elif isinstance(error.original, commands.BotMissingPermissions):
-			await ctx.respond("Fritz has insufficient permission to execute the command. Please re-invite Fritz and grant all requested permissions")
+			await ctx.respond("Fritz has insufficient permission to execute the command. Please re-invite Fritz and grant all requested permissions", ephemeral=True)
 
 		elif isinstance(error.original, bannedUser):
 			journal.log(f"Blacklisted user attempted to use Fritz: {str(error)}", 5)
-			await ctx.respond("You are blacklisted from using Fritz")
+			await ctx.respond("You are blacklisted from using Fritz", ephemeral=True)
 
 			return
 
 		elif isinstance(error.original, commands.errors.CheckFailure):
 			journal.log(f"Generic check failure detected: {str(error)}", 5)
-			await ctx.respond('Failed one or more command checks - you are not allowed to run this command')
+			await ctx.respond('Failed one or more command checks - you are not allowed to run this command', ephemeral=True)
 
 		elif isinstance(error.original, commands.errors.MissingRequiredArgument):
-			await ctx.respond(f"Looks like you forgot to provide a value for {str(error)}. Try filling out all required fields first!")
+			await ctx.respond(f"Looks like you forgot to provide a value for {str(error)}. Try filling out all required fields first!", ephemeral=True)
 
 		# === Extensions ===
 		elif isinstance(error.original, discord.ExtensionNotFound):
 			journal.log("Extension {error.original.name} not found", 3)
-			await ctx.respond(view=ManagementView(f"Extension `{error.original.name}` not found", title="Load Plugin", success=False))
+			await ctx.respond(view=ManagementView(f"Extension `{error.original.name}` not found", title="Load Plugin", success=False), ephemeral=True)
 
 		elif isinstance(error.original, discord.ExtensionNotLoaded):
 			journal.log(f"Extension {error.original.name} not loaded", 4)
-			await ctx.respond(view=ManagementView(f"Extension `{error.original.name}` not loaded", title="Load Plugin", success=False))
+			await ctx.respond(view=ManagementView(f"Extension `{error.original.name}` not loaded", title="Load Plugin", success=False), ephemeral=True)
 
 		elif isinstance(error.original, discord.ExtensionAlreadyLoaded):
 			journal.log(f"Extension {error.original.name} already loaded", 4)
-			await ctx.respond(view=ManagementView(f"Extension `{error.original.name}` already loaded", title="Load Plugin", success=False))
+			await ctx.respond(view=ManagementView(f"Extension `{error.original.name}` already loaded", title="Load Plugin", success=False), ephemeral=True)
 
 		elif isinstance(error.original, discord.NoEntryPointError):
 			journal.log(f"Extension {error.original.name} has no entrypoint", 3)
-			await ctx.respond(view=ManagementView(f"Extension `{error.original.name}` failed to load: No entrypoint", title="Load Plugin", success=False))
+			await ctx.respond(view=ManagementView(f"Extension `{error.original.name}` failed to load: No entrypoint", title="Load Plugin", success=False), ephemeral=True)
 
 		elif isinstance(error.original, discord.ExtensionFailed):
 			journal.log(f"Extension {error.original.name} failed to load: {str(error.original.original)}", 3)
-			await ctx.respond(view=ManagementView(f"Extension `{error.original.name}` failed to load: `{str(error.original.original)}", title="Load Plugin", success=False))
+			await ctx.respond(view=ManagementView(f"Extension `{error.original.name}` failed to load: `{str(error.original.original)}", title="Load Plugin", success=False), ephemeral=True)
 
 		else:
 			journal.log("Failed to execute command: " + str(error), 3)
-			await ctx.respond(f"Failed to execute command. Please [report this bug](<{GIT_URL}/issues/new?assignees=&labels=bug%2Cbroken+command&projects=&template=broken_command.yml>)")
+			await ctx.respond(f"Failed to execute command. Please [report this bug](<{GIT_URL}/issues/new?assignees=&labels=bug%2Cbroken+command&projects=&template=broken_command.yml>)", ephemeral=True)
 
 	except Exception as err:
 		journal.log(f"While handling an error, another one occured: {str(err)}", 3)
